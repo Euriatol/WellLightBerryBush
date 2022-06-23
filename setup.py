b8e7e200c9c175e140c01e53dc0943a9bc7a3c57
Euriatol <49067036+Euriatol@users.noreply.github.com>
2022-06-23 21:18:33 +0200

    Creating a setup.py file for the package

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="wlbb",
+    version="0.0.1",
+    author="Lothaire Sicot",
+    author_email='euriatol@gmail.com',
+    url="https://github.com/Euriatol/pyWellLightBerryBush",
+    description="WLBB's main purpose is controlling LEDs located in a 3D environement.",
+    packages=["wlbb"],
+    python_requires='>=3.6',
+    install_requires=[] # This will problably change
+)
