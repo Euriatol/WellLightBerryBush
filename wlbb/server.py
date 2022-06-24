00be2499b8aa371db3de8c942557eb33049556a8
Lothaire Sicot <euriatol@gmail.com>
2022-06-24 09:55:16 +0200

    Starting implementation
    Starting the implementation of multiple module. config.py should be fully implemented.

---
+++ wlbb/server.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+server.py
+"""
+
+
+class WLBBServer:
+    def __init__(self,    ):
+        pass
+    
+    def init(self):
+        pass
+    
+    def mainloop(self):
+        pass
+    
+    def start(self):
+        pass
+    
+    def stop(self):
+        pass
+    
+    def restart(self):
+        pass
+    
+    def get_config(self):
+        pass
+    
+    def load_config(self, new_config):
+        pass
+    
+    def reload(self):
+        pass
